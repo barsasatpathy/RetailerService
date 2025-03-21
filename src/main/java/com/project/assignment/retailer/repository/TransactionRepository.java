3e8a69abb15b5deb760ae769048551c229294c0e
Satpathy <barsa.satpathy@capgemini.com>
2025-03-21 17:57:07 +0530

    Code changes for retailer assignment

--- src/main/java/com/project/assignment/retailer/repository/TransactionRepository.java
+++ src/main/java/com/project/assignment/retailer/repository/TransactionRepository.java
@@ -3,7 +3,6 @@ package com.project.assignment.retailer.repository;
 import com.project.assignment.retailer.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
